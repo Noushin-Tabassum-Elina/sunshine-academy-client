cfb80813457a40138978e40f7376fd7c8e0d1b4b
Noushin-Tabassum-Elina <elinajsr05@gmail.com>
2023-06-08 10:42:13 +0600

    Error page added

---
+++ src/pages/Error/Error.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div  className='bg-gray-100 
+        flex justify-center items-center h-screen
+    '>
+            <img src="https://i.ibb.co/25fgNWY/funny-404-error-page-design.gif" alt="" />
+        </div>
+    );
+};
+
+export default Error;
