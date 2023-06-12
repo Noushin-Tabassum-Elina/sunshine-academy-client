bc6c6ce610484fc848c366b9863346376d02d1eb
Noushin-Tabassum-Elina <elinajsr05@gmail.com>
2023-06-12 22:16:41 +0600

    useAdmin hook added

--- src/hooks/useAdmin.jsx
+++ src/hooks/useAdmin.jsx
@@ -10,12 +10,12 @@ const useAdmin = () => {
     const [axiosSecure] = useAxiosSecure();
     const { data: isAdmin, isLoading: isAdminLoading } = useQuery({
         queryKey: ['isAdmin', user?.email],
+        
         queryFn: async () => {
             const res = await axiosSecure.get(`/users/admin/${user?.email}`);
             return res.data.admin;
         }
     })
-    
     return [isAdmin, isAdminLoading];
 }
 export default useAdmin;
